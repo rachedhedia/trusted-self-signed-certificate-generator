031dc4547fc5f000eaf66019b8446c1ea7515177
gatoos software <gatoos.software@gmail.com>
2019-05-07 01:59:19 +0200

    Update the certificate generation script to use the docker hub published image

--- generate-certificates.cmd
+++ generate-certificates.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-docker run --rm -it -v %CD%:/export trusted-self-signed-certificate-generator sh startup.sh
+docker run --rm -it -v %CD%:/export rhedia/trusted-self-signed-certificate-generator sh startup.sh
 
 echo Add RootCA.pem to your list of trusted certificate authorities
 echo Use server.crt and server.key on your local server
