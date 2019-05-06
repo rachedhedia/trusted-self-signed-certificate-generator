b71cb0049b972eae060a3e94455fd5d0ed18d58d
gatoos software <gatoos.software@gmail.com>
2019-05-07 01:13:40 +0200

    Create the certificate generation script + docker file

---
+++ src/startup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/dakshshah96/local-cert-generator.git
+cd local-cert-generator/
+sh createRootCA.sh 
+sh createSelfSigned.sh 
+cp rootCA.pem /export 
+cp server.key /export 
+cp server.csr /export 
