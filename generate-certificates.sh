cb7b55537a83ab1bc0323c07c2dceba12d61e840
gatoos software <gatoos.software@gmail.com>
2019-05-07 02:10:15 +0200

    Add unix generate-certificates script

---
+++ generate-certificates.sh
@@ -0,0 +1,4 @@
+docker run --rm -it -v $PWD:/export rhedia/trusted-self-signed-certificate-generator sh startup.sh
+
+echo Add RootCA.pem to your list of trusted certificate authorities
+echo Use server.crt and server.key on your local server
